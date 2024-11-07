09d888ecc9649a131f2b77275978d1d42b4007b5
nathan95oks <nathanaelalba75@gmail.com>
2024-11-06 23:14:50 -0400

    suma de n numeros con - funcional con cy y test

--- src/presenter.js
+++ src/presenter.js
@@ -1,5 +1,8 @@
 document.getElementById('calculateBtn').addEventListener('click', () => {
     const input = document.getElementById('input').value;
+    console.log("Calculando con cadena:", input);  
     const result = calcularCadena(input);  
     document.getElementById('result').textContent = result;
 });
+
+
